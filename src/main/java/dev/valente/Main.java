1aec6a0d4cf833d70415b4e2d6f4c8a3747136db
Gabriel Valente <112831085+ValentePG@users.noreply.github.com>
2024-10-31 16:55:14 -0300

    Teste rabbit com exchange

--- src/main/java/dev/valente/Main.java
+++ src/main/java/dev/valente/Main.java
@@ -3,6 +3,8 @@ package dev.valente;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import dev.valente.rabbitmq.RabbitProducer;
+
 
 import java.io.IOException;
 import java.util.Scanner;
@@ -12,6 +14,10 @@ public class Main {
     public static void main(String[] args) throws IOException, TimeoutException {
 
         Scanner scanner = new Scanner(System.in);
+        RabbitProducer producer = new RabbitProducer();
+
+
+
 
         System.out.print("Escreva uma mensagem para o sistema consumidor:");
         String inputUser;
@@ -20,16 +26,32 @@ public class Main {
 
         factory.setHost("localhost");
 
-        do {
-            inputUser = scanner.nextLine();
+
+
             try (Connection connection = factory.newConnection();
                  Channel channel = connection.createChannel()){
 
-                channel.queueDeclare("hello", false, false, false, null);
-                channel.basicPublish("", "hello", null, inputUser.getBytes());
-                System.out.println(" [x] Sent ´" + inputUser + "´");
+
+
+                String exchangeName = "Teste";
+                String queueName = "hello";
+                String routingKey = "algumaroutingkey";
+
+                channel.exchangeDeclare(exchangeName, "direct", true);
+                channel.queueDeclare(queueName, false, false, false, null);
+                channel.queueBind(queueName, exchangeName, routingKey);
+
+               byte[] messageBodyBytes = producer.toString().getBytes();
+               channel.basicPublish(exchangeName, routingKey, null, messageBodyBytes);
+
+
+
+//                channel.queueDeclare("hello", false, false, false, null);
+//                channel.basicPublish("", "hello", null, inputUser.getBytes());
+//                System.out.println(" [x] Sent ´" + inputUser + "´");
+
             }
-        }  while(!inputUser.equals("0"));
+
 
 
     }
--- src/main/java/dev/valente/rabbitmq/RabbitProducer.java
+++ src/main/java/dev/valente/rabbitmq/RabbitProducer.java
@@ -1,5 +1,14 @@
 package dev.valente.rabbitmq;
 
 public class RabbitProducer {
+    public String teste = "Isso é um teste";
+    public int testeInt = 5;
 
+    @Override
+    public String toString() {
+        return "RabbitProducer{" +
+                "teste='" + teste + '\'' +
+                ", testeInt=" + testeInt +
+                '}';
+    }
 }
